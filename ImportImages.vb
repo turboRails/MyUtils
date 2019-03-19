b36264952845d371248348de2be4a18167ebe1fa
Upendra N <turboRails@users.noreply.github.com>
2019-03-19 10:56:00 +0900

    Update ImportImages.vb

--- ImportImages.vb
+++ ImportImages.vb
@@ -1,28 +1,34 @@
 Sub Insert()
   Dim strFolder As String
   Dim strFileName As String
-  Dim objPic As Picture
+  Dim shpPicture As Shape
+  Dim strFilePath As String
+    
   Dim rngCell As Range
   Dim H As Integer
   Dim Top As Integer
-  strFolder = "C:\Users\msc\Pictures\Screenshots\遺伝子試験項目\" 'change the path accordingly
+  strFolder = "C:\Users\" & Environ$("Username") & "\Pictures\Screenshots"
+  
   If Right(strFolder, 1) <> "\" Then
       strFolder = strFolder & "\"
   End If
-  Set rngCell = Range("B1") 'starting cell
-  strFileName = Dir(strFolder & "*.png", vbNormal) 'filter for .png files
-  H = 500
+
+  strFileName = Dir(strFolder & "*.png", vbNormal)
+  ExtraSpace = 100
   Top = 50
   Do While Len(strFileName) > 0
-      Set objPic = ActiveSheet.Pictures.Insert(strFolder & strFileName)
-      With objPic
-          .Left = rngCell.Left
-          .Top = Top
-          .Height = H
-          .Placement = xlMoveAndSize
-          Top = Top + H + 50
-      End With
-      Set rngCell = rngCell.Offset(1, 0)
-      strFileName = Dir
+  
+    strFilePath = strFolder & strFileName
+    Set shpPicture = ActiveSheet.Shapes.AddPicture( _
+    Filename:=strFolder & strFileName, _
+    LinkToFile:=False, _
+    SaveWithDocument:=True, _
+    Left:=Selection.Left, _
+    Top:=Top, _
+    Width:=-1, _
+    Height:=-1)
+    
+    Top = Top + ExtraSpace + shpPicture.Height
+    strFileName = Dir
   Loop
 End Sub
